95c5a51bef21c9896372784e8a913d5fb00d13a0
Sunrise-Lecybyl <hieronim.lecybyl@oriel.ox.ac.uk>
2025-03-24 12:00:36 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,3 @@
-/**
- * Functions are mapped to blocks using various macros
- * in comments starting with %. The most important macro
- * is "block", and it specifies that a block should be
- * generated for an **exported** function.
- */
-
-//% color="#AA278D" weight=100
 namespace graphing {
 
 
@@ -13,10 +5,33 @@ namespace graphing {
     // block text with spaces
 
     //% block
-    export function lineGraph(x_list: Array<Number>, y_list: Array<Number>) {
+    function normaliseArray(xs: Array<number>, mini: number, maxi: number) {
+        let xs_mini = xs.reduce((a, b) => Math.min(a, b), 0);
+        let xs_maxi = xs.reduce((a, b) => Math.max(a, b), 0);
+        let index = 0;
+        let new_xs: Array<number>;
+        while (index < xs.length) {
+            new_xs[index] = (xs[index] - xs_mini) * (maxi - mini) / (xs_maxi - xs_mini)
+            index = index + 1;
+        }
+        return new_xs
+    }
+    export function lineGraph(x_list: Array<number>, y_list: Array<number>) {
+        let new_x_list = normaliseArray(x_list, 5, 154);
+        let new_y_list = normaliseArray(x_list, 5, 114);
         screen().fill(1);
         console.log(x_list);
         console.log(y_list);
+        screen().drawLine(5, 5, 5, 114, 0)
+        screen().drawLine(5, 114, 154, 114, 0)
+        let index = 1;
+        let point = [x_list[0], y_list[0]];
+        let new_point: [number, number]
+        while (index < Math.min(x_list.length, y_list.length)) {
+            new_point = [x_list[index], y_list[index]];
+            screen().drawLine(point[0], point[1], new_point[0], new_point[1], 2);
+            index = index + 1;
+        }
         return;
 
     }
