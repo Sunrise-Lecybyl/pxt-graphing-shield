046e45e8907ec3abeb753ba8cb8e7da762e1a9d0
Sunrise-Lecybyl <hieronim.lecybyl@oriel.ox.ac.uk>
2025-05-14 03:52:37 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -95,7 +95,7 @@ namespace graphing {
                 screen().drawLine(80, 60, 80 + 50 * Math.sin(angle), 60 + 50 * Math.cos(angle), colour); //no good fill function so just draws a ton of lines until hopefully every point is covered.
                 screen().drawLine(midx, midy, 80 + 50 * Math.sin(angle), 60 + 50 * Math.cos(angle), colour);
                 screen().drawLine((80+midx)*0.5, (60+midy)*0.5, 80 + 50 * Math.sin(angle), 60 + 50 * Math.cos(angle), colour);
-                //screen().drawLine(80 + 25 * Math.sin(dir), 60 + 25 * Math.cos(dir), 80 + 50 * Math.sin(angle), 60 + 50 * Math.cos(angle), colour);
+                screen().drawLine(80 + 25 * Math.sin(dir), 60 + 25 * Math.cos(dir), 80 + 50 * Math.sin(angle), 60 + 50 * Math.cos(angle), colour);
                 //screen().drawLine(80 + 25 * Math.sin(new_dir), 60 + 25 * Math.cos(new_dir), 80 + 50 * Math.sin(angle), 60 + 50 * Math.cos(angle), colour);
             }
             colour++;
